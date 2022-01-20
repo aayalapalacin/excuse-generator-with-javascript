a5ba3fc6104295f56d8e6080768b1905e1a3badf
avokeys <alexander.ayala.palacin@gmail.com>
2022-01-20 17:19:40 +0000

    finished excuse generator

--- src/app.js
+++ src/app.js
@@ -2,10 +2,32 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+window.onload = () => {
+  document.querySelector("#excuseButton").addEventListener("click", () => {
+    document.querySelector("#the-excuse").innerHTML = generateExcuse();
+  });
+};
+let generateExcuse = () => {
+  let pro = ["A", "The"];
+  let subj = ["jogger", "racoon", "driver", "dog", "comedian", "pinecone"];
+  let act = ["took my", "threw my", "yelled at my", "stole my", "bit my"];
+  let pos = ["toe", "homework", "car", "shoe"];
+  let wh = ["on the street", "in my house", "in my driveway"];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let proIndex = Math.floor(Math.random() * pro.length);
+  let subIndex = Math.floor(Math.random() * subj.length);
+  let actIndex = Math.floor(Math.random() * act.length);
+  let posIndex = Math.floor(Math.random() * pos.length);
+  let whIndex = Math.floor(Math.random() * wh.length);
+  return (
+    pro[proIndex] +
+    " " +
+    subj[subIndex] +
+    " " +
+    act[actIndex] +
+    " " +
+    pos[posIndex] +
+    " " +
+    wh[whIndex]
+  );
 };
